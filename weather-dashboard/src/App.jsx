8b38567c7d2067acb6113b1e5f665ceb92cf5558
an026 <allisonvu9@gmail.com>
2024-04-01 00:09:54 -0400

    Final.

--- weather-dashboard/src/App.jsx
+++ weather-dashboard/src/App.jsx
@@ -1,14 +1,49 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
-const API_KEY = import.meta.env.VITE_APP_API_KEY
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Sidebar from './Components/Sidebar.jsx';
+// import './App.css';
+// import React, { useState, useEffect } from 'react';
+import Dashboard from './Dashboard.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-   
-  )
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    // declare the async data fetching function
+    const fetchData = async () => {
+
+      const location = 'New York, NY';
+      const API_KEY = '3C3DR7FPTDDTCSHRPTT4VBCZP';
+      const options = '&include=days&elements=id,temp,feelslikemin,tempmin,datetime,moonphase,sunrise,sunset,moonrise,moonset,description,visibility,conditions'
+      const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+ location + '?key='+ API_KEY + options;
+
+      const response = await fetch(url);
+      const json = await response.json();
+
+      setData(json.days);
+
+      return json;
+    }
+
+    fetchData()
+
+  }, []);
+
+
+  return ( 
+
+    <div className="App">
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+
+      <Dashboard data={data}/>
+      
+    </div>
+
+  );
 }
 
-export default App
+export default App;
